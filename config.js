a2eb60f57d3b6e5833dbf68642fe5f5d4b0f6009
hemal <hemal@mail.ru>
2017-02-07 09:25:45 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by vhn on 06/02/2017.
+ */
+
+
+module.exports = {
+    'port': process.env.PORT || 8081
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+/**
+ * Created by vhn on 06/02/2017.
+ */
+
+var express     = require('express');
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var path        = require('path');
+var mongoose    = require('mongoose');
+
+var config      = require('./config');
+
+var app         = express();
+
+// APP CONFIGURATION ==================
+// ====================================
+// use body parser so we can grab information from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
+// log all requests to the console
+app.use(morgan('dev'));
+
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/public'));
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
+
+/********************************************
+*             START SERVER                  *
+******************************************* */
+
+app.listen(config.port);
+console.log('Listening on port ' + config.port);
